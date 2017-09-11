e7bbabaf0577d2d24462f2a1243a844650beccf4
James Geng <james.geng@famoco.com>
2017-09-12 00:21:48 +0800

    push to redis

--- rest.py
+++ rest.py
@@ -1,15 +1,12 @@
 import time
 import json
 import requests
+import redis
 from exchange_rate_boc import fetch_exchange_rates
 
-ES_POST_URL = 'http://192.168.1.100:9200/forex/boc'
-
 if __name__ == '__main__':
-	rates = fetch_exchange_rates()
-	for rate in rates:
-		rate['@timpstamp'] = time.strftime("%Y-%m-%d %H:%M:%S")
-		rate['@version'] = 1
-		payload = json.dumps(rate)
-		resp = requests.post(ES_POST_URL, data=payload)
-		print(resp.text)
+    rates = fetch_exchange_rates()
+    r = redis.StrictRedis(host='10.0.0.30', port=6379, db=0)
+    for rate in rates:
+        payload = json.dumps(rate)
+        r.lpush('logstash', payload)

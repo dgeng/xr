2ce4dfb7faf6074a95f07d6567902067fc989f00
James Geng <james.geng@famoco.com>
2017-09-14 23:34:02 +0800

    complete dockerization of crawler

--- crawler/app/crawler.py
+++ crawler/app/crawler.py
@@ -1,17 +1,27 @@
-import schedule
 import time
+import json
+import requests
+import schedule
+from exchange_rate_boc import fetch_exchange_rates
+
 
 def job():
-    f = open('/data/my.log', 'a')
-    f.write('i am working\n')
-    f.close()
+    rates = fetch_exchange_rates()
+    for rate in rates:
+        payload = json.dumps(rate)
+        f = open('/data/crawler.log', 'a')
+        f.write(payload); f.write('\n')
+        f.close()
 
-schedule.every(1).seconds.do(job)
-schedule.every().hour.do(job)
-schedule.every().day.at("10:30").do(job)
-schedule.every().monday.do(job)
-schedule.every().wednesday.at("13:15").do(job)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+schedule.every(1).minutes.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+
+if __name__ == '__main__':
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
